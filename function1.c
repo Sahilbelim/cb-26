e384507e50e01e45b4da6adf3da12e12802a3de8
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-12-05 19:10:36 +0530

    funtion start

---
+++ function1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void printline()
+{
+    printf("\n----------------------------------------------\n");
+    printf("----------------------------------------------\n");
+    printf("----------------------------------------------\n\n");
+}
+
+void main()
+{
+    printline();
+    printf("\tName : Sahil Belim \n");
+    printline();
+    printf("\tAge : 18 \n");
+    printline();
+    printf("\tNumber : 9924254332\n");
+    printline();
+    printf("\tEmail: Sahil@gmail.com\n");
+    printline();
+}
